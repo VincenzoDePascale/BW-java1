7b3ddc95ca9207f92d3c3fd39410afdc75680c84
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-04-04 15:33:54 +0200

    .

--- src/titoli/TitoliDAO.java
+++ src/titoli/TitoliDAO.java
@@ -20,54 +20,54 @@ public class TitoliDAO {
 			A1.setData_emissione(LocalDate.of(2023, 4, 4));
 			A1.setDurata(TipoAbbonamento.MENSILE);
             A1.setData_inizio(LocalDate.of(2020, 2, 7), A1.getDurata());
-			System.out.println(A1.toString());
-			addTitolo(A1);
+			//System.out.println(A1.toString());
+			//addTitolo(A1);
 			
 			Biglietto B1 = new Biglietto();
 			B1.setData_emissione(LocalDate.of(2023, 4, 4));
-			System.out.println(B1.toString());
-			addTitolo(B1);
+			//System.out.println(B1.toString());
+			//addTitolo(B1);
 			
 			Abbonamento A2 = new Abbonamento();
             A2.setData_emissione(LocalDate.of(2023, 3, 28));
             A2.setDurata(TipoAbbonamento.SETTIMANALE);
             A2.setData_inizio(LocalDate.of(2020, 2, 7), A2.getDurata());
-            System.out.println(A2.toString());
-            addTitolo(A2);
+            //System.out.println(A2.toString());
+            //addTitolo(A2);
 
             Biglietto B2 = new Biglietto();
             B2.setData_emissione(LocalDate.of(2023, 4, 3));
-            System.out.println(B2.toString());
-            addTitolo(B2);
+            //System.out.println(B2.toString());
+            //addTitolo(B2);
             
             Abbonamento A3 = new Abbonamento();
             A3.setData_emissione(LocalDate.of(2023, 2, 28));
             A3.setDurata(TipoAbbonamento.SETTIMANALE);
             A3.setData_inizio(LocalDate.of(2020, 2, 7), A3.getDurata());
-            System.out.println(A3.toString());
-            addTitolo(A3);
+            //System.out.println(A3.toString());
+            //addTitolo(A3);
 
             Biglietto B3 = new Biglietto();
             B3.setData_emissione(LocalDate.of(2023, 5, 3));
-            System.out.println(B3.toString());
-            addTitolo(B3);
+            //System.out.println(B3.toString());
+            //addTitolo(B3);
             
             Abbonamento A4 = new Abbonamento();
             A4.setData_emissione(LocalDate.of(2020, 2, 7));
             A4.setDurata(TipoAbbonamento.MENSILE);
             A4.setData_inizio(LocalDate.of(2020, 2, 7), A4.getDurata());
-            System.out.println(A4.toString());
-            addTitolo(A4);
+            //System.out.println(A4.toString());
+            //addTitolo(A4);
 
             Biglietto B4 = new Biglietto();
             B4.setData_emissione(LocalDate.of(2022,5,6));
-            System.out.println(B4.toString());
-            addTitolo(B4);
+            //System.out.println(B4.toString());
+            //addTitolo(B4);
             
            B1.setVidimato(true);
-           System.out.println(B1.toString());
+           //System.out.println(B1.toString());
 			
-          Titolo letto = serchByNum(2l);
+           //Titolo letto = serchByNum(2l);
           
             
 		}catch(Exception e){
