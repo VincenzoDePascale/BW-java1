53d1b7e6858f52f204259d336b80100ac016ca90
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-04-04 11:11:06 +0200

    change main to Main and added "BW-java1"

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,14 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class main {
+import utenti.Utente;
+
+public class Main {
 	
-	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("W3-weeklyProject-java");
+	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-java1");
 	static EntityManager em = emf.createEntityManager();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		Utente utente1 = new Utente();
+		
+		
+		
 	}
 
 }
