609f5c675211654b04fac08eb5edcbcea1e08d45
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-04-04 11:15:50 +0200

    add class

--- src/main/main.java
+++ src/main/main.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class main {
+public class Main {
 	
-	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("W3-weeklyProject-java");
+	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-java1");
 	static EntityManager em = emf.createEntityManager();
 
 	public static void main(String[] args) {
---
+++ src/registri/Registro_viaggi.java
@@ -0,0 +1,65 @@
+package registri;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+import mezzi.ParcoMezzi;
+import tratte.Tratta;
+
+@Entity
+@Table(name = "registri")
+public class Registro_viaggi extends Registro {
+	
+	@Column
+	private LocalDate init_viaggio;
+	
+	@Column
+	private LocalDate fine_viaggio;
+	
+	@Column
+	private Tratta tratta;
+	
+	public Registro_viaggi() {}
+
+	public Registro_viaggi(ParcoMezzi mezzo, LocalDate init_viaggio, LocalDate fine_viaggio, Tratta tratta) {
+		super(mezzo);
+		this.init_viaggio = init_viaggio;
+		this.fine_viaggio = fine_viaggio;
+		this.tratta = tratta;
+	}
+
+	public LocalDate getInit_viaggio() {
+		return init_viaggio;
+	}
+
+	public void setInit_viaggio(LocalDate init_viaggio) {
+		this.init_viaggio = init_viaggio;
+	}
+
+	public LocalDate getFine_viaggio() {
+		return fine_viaggio;
+	}
+
+	public void setFine_viaggio(LocalDate fine_viaggio) {
+		this.fine_viaggio = fine_viaggio;
+	}
+
+	public Tratta getTratta() {
+		return tratta;
+	}
+
+	public void setTratta(Tratta tratta) {
+		this.tratta = tratta;
+	}
+
+	@Override
+	public String toString() {
+		return "Registro_viaggi [init_viaggio=" + init_viaggio + ", fine_viaggio=" + fine_viaggio + ", tratta=" + tratta
+				+ "]";
+	}
+
+	
+		
+	
+}
