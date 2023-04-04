44a301a2fdb822af99278bfeba1ed41933bdd245
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-04-04 13:02:39 +0200

    fix entity manager factory line 10

--- src/utenti/UtentiDAO.java
+++ src/utenti/UtentiDAO.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 
 public class UtentiDAO {
 	
-	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("W3-weeklyProject-java");
+	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-java1");
 	static EntityManager em = emf.createEntityManager();
 
 	public static void main(String[] args) {
